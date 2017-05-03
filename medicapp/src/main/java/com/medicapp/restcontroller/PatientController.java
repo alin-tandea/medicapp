3957736de246c0cea7f28370ad78ad53b0dde625
Alin Tandea <alin3011@gmail.com>
2017-05-03 15:56:50 +0300

    rest services kinda working

--- medicapp/src/main/java/com/medicapp/restcontroller/PatientController.java
+++ medicapp/src/main/java/com/medicapp/restcontroller/PatientController.java
@@ -2,16 +2,30 @@ package com.medicapp.restcontroller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import java.util.List;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.medicapp.data.model.Patient;
 import com.medicapp.service.PatientService;
 
-@Path("/patient")
+
+
+@Path("/patients")
 public class PatientController {
 	
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
 	@GET
-	public List<Patient> getPatients(){
-		return PatientService.getAllPatients();
+	@Path("/all")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response getPatients(){
+		for(Patient patient : PatientService.getAllPatients()){
+			System.out.println(patient.getName());
+		}
+		return Response.ok("abc").build();
 	}
 }
